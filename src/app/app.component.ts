9a718dd090514eacdf6de32920de22974f84ed64
arhamabeer <arhamabeerahmed@gmail.com>
2021-09-17 18:21:43 +0500

    working calculator developed...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  num = ''
+  constructor(){}
+  btnClick(i:any, e:any) {
+    // console.log(e)
 
-  btnClick(e:number) {
-    console.log(e)
-  }
-  calculate(){
-    let inp = document.getElementById('inp')
-    console.log(inp.native)
-  }
+    this.num = this.num + e
+    i.value = this.num
 
+    // var inp = document.getElementById('inp')?.value()
+    // inp = 'anc'
+    // console.log(i)
+  }
+  calculate(e: any){
+    // console.log(e.value)
+    this.num = eval(this.num)
+    e.value = this.num
+    // console.log(this.num)
+    this.num= ''
+  }
+  clear(e:any){
+    this.num = ""
+    e.value = 0
+  }
 
 }
